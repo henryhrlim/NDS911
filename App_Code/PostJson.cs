d8af118c5ea986db1997ea90967e5082f0c977b7
henryhrlim <33216909+henryhrlim@users.noreply.github.com>
2018-03-31 23:17:26 +0800

    upload#7

---
+++ App_Code/PostJson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PostJson
+/// </summary>
+public class PostJson
+{
+    public PostJson()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        String CallerName;
+        String CallerNric;
+        int CallBackNo;
+        String LocationofEmergency;
+        TimeSpan TimeOfEmergency;
+        String TypeOfSuperBeing;
+        int EstimatedNoOfBeings;
+        int EstimatedNoOfInjuries;
+        int EstimatedNoOfDeath;
+        String DescOfEmergency;
+        String StaffId;
+        String AlertedCMOId;
+        TimeSpan IncidentCreatedTime;
+        DateTime IncidentCreatedDate;
+        String NatureOfEmergency;
+        String NameOfEmergency;
+
+
+
+
+
+    }
+}
---
+++ App_Code/Report911insert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Report911insert
+/// </summary>
+public class Report911insert
+{
+   
+    public string CallerName { get; set; }
+    public string CallerNRIC { get; set; }
+    public int CallBackNo { get; set; }
+    public string LocationofEmergency { get; set; }
+    public DateTime DatetimeOfEmergency { get; set; }
+    public string TypeOfSuperBeing { get; set; }
+    public int EstimatedNoOfBeings { get; set; }
+    public int EstimatedNoOfInjuries { get; set; }
+    public int EstimatedNoOfDeath { get; set; }
+    public string DescOfEmergency { get; set; }
+    public string StaffId { get; set; }
+    public string AlertedCMOId { get; set; }
+    public DateTime IncidentCreatedDatetime { get; set; }
+    public string NatureOfEmergency { get; set; }
+    public string NameOfEmergency { get; set; }
+}
